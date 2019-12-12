1ee1b48706ae4a6e7e8fce83c90c4564b2ecb276
Christian Kohout <christian.kohout@gmx.at>
2019-12-12 09:20:17 +0100

    Slight change

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('The final exercise by Christian Kohout');
+  res.send('The final exercise by Christian Kohout!');
 });
 
 app.listen(PORT, HOST);
